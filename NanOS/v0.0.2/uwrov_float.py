f891c4afe5e9cb6b833940a46f88a8a36201db97
EmilyMeng05 <160436343+EmilyMeng05@users.noreply.github.com>
2024-04-14 15:40:38 -0700

    Add files via upload
    This is a temp file for the future float and uwrov user interaction method.

---
+++ NanOS/v0.0.2/uwrov_float.py
@@ -0,0 +1,20 @@
+# TEMP FILE FOR THE FLOAT AND UWROV USER INTERACTION METHOD
+
+MENU()
+
+def MENU():
+    print("Float dive down")
+    print("Float dive up")
+    function = input()
+
+    if function == "Float dive down":
+        float_dive_down()
+    elif function == "Float dive up":
+        float_dive_up()
+    
+
+def float_dive_down():
+    print("temp method")
+
+def float_dive_up():
+    print("temp method")
